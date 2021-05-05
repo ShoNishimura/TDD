506f50c2c8de5e85f144a853b6a4350436bfcdeb
shonishimura <shonishimura.dev@gmail.com>
2021-05-05 22:19:54 +0900

    Dollar, Francサブクラスを削除

--- money/src/test/java/money/MoneyTest.java
+++ money/src/test/java/money/MoneyTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MoneyTest {
+
     @Test
     public void testMultiplication(){
         Money five = Money.dollar(5);
@@ -14,8 +15,6 @@ public class MoneyTest {
     public void testEquality(){
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
     @Test
