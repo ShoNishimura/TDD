2784795ac1bbc12601036f0b5b72353ecf37695a
shonishimura <shonishimura.dev@gmail.com>
2021-05-05 22:16:29 +0900

    Dollar, FrancサブクラスをMoney親クラスに引き上げ

--- money/src/main/java/money/Dollar.java
+++ money/src/main/java/money/Dollar.java
@@ -4,9 +4,5 @@ class Dollar extends Money{
     Dollar(int amount, String currency){
         super(amount, currency);
      }
-    
-    Money  times(int multiplier){
-        return Money.dollar(amount * multiplier);
-    }
 }
 
