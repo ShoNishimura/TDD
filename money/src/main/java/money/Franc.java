9949c70724498fb049a66fbaa40571332a949c19
shonishimura <shonishimura.dev@gmail.com>
2021-05-05 17:00:44 +0900

    Francクラスのequalメソッドを重複のため削除

--- money/src/main/java/money/Franc.java
+++ money/src/main/java/money/Franc.java
@@ -7,8 +7,4 @@ class Franc extends Money{
     Franc times(int multiplier){
         return new Franc(amount * multiplier);
     }
-    public boolean equals(Object object){
-        Franc franc =(Franc) object;
-        return amount == franc.amount;
-    }
 }
