ff6b62ee493a1b6dc6c0d8203be72c7e9f60ccb0
shonishimura <shonishimura.dev@gmail.com>
2021-05-06 14:53:49 +0900

    足し算のテストtestSimpleAdditionを追加

--- money/src/test/java/money/MoneyTest.java
+++ money/src/test/java/money/MoneyTest.java
@@ -7,8 +7,11 @@ public class MoneyTest {
 
     @Test 
     public void testSimpleAddtion(){
-        Money sum = Money.dollar(5).plus(Money.dollar(5));
-        assertEquals(Money.dollar(10), sum);
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
     }
 
     @Test
@@ -17,6 +20,7 @@ public class MoneyTest {
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15),five.times(3));
     }    
+
     @Test
     public void testEquality(){
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
@@ -29,4 +33,5 @@ public class MoneyTest {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
     }
+
 }
