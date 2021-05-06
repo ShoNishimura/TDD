00b7f1c0180d8982abfbbb0a4f6da7a4b9e8447f
shonishimura <shonishimura.dev@gmail.com>
2021-05-06 13:39:25 +0900

    足し算plusメソッド追加

--- money/src/test/java/money/MoneyTest.java
+++ money/src/test/java/money/MoneyTest.java
@@ -5,6 +5,12 @@ import static org.junit.Assert.*;
 
 public class MoneyTest {
 
+    @Test 
+    public void testSimpleAddtion(){
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
+    }
+
     @Test
     public void testMultiplication(){
         Money five = Money.dollar(5);
